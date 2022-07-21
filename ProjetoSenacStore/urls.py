1696a75024240d3f7987a3959f6aeb0770b6d67b
jefferson.santos <jeffersongb0@gmail.com>
2022-07-20 22:27:12 -0300

    NOVO ACESSO URL

--- ProjetoSenacStore/urls.py
+++ ProjetoSenacStore/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from xml.dom.minidom import Document
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('Store.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Store/views.py
+++ Store/views.py
@@ -1,3 +1,8 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
-
 # Create your views here.
+def index(request):
+    return HTTPResponse('hello word!')
+
+def teste(request):
+    return HTTPResponse('minha pagina de teste')
