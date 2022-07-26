74e1f5396998ec55a1b5404990a45702f4c73956
jefferson.santos <jeffersongb0@gmail.com>
2022-07-25 22:18:00 -0300

    templates html

--- Store/urls.py
+++ Store/urls.py
@@ -1,7 +1,7 @@
 
 
 from django.urls import path
-from . import views
+from Store import views
 
 urlpatterns = [
     path('', views.index, name='index'),
--- Store/views.py
+++ Store/views.py
@@ -1,8 +1,17 @@
 from http.client import HTTPResponse
+from multiprocessing import context
 from django.shortcuts import render
+from Store.models import Departamento
+
 # Create your views here.
 def index(request):
-    return HTTPResponse('hello word!')
+    meu_nome = "Jefferson Gonçalves Dos Santos"
+    sexo = "a"
+    context = {'nome': meu_nome, "artigo": "o" if sexo=="m" else "a"}
+    return render(request,'index.html',context)
 
 def teste(request):
-    return HTTPResponse('minha pagina de teste')
+    # depto = ['Casa','Informatica','Telefonia','Gamer']
+    depto = Departamento.objects.all()
+    context = {'departamentos':depto}
+    return render(request,'teste.html',context)
