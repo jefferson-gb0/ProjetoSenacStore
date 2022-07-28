d2b1c9d5ca33277a144b99eba479e703e6232cd9
jefferson.santos <jeffersongb0@gmail.com>
2022-07-27 22:27:05 -0300

    tamplates_

--- Store/urls.py
+++ Store/urls.py
@@ -1,9 +1,13 @@
 
 
+from unicodedata import name
 from django.urls import path
 from Store import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('teste', views.teste, name='teste')
+    # path('teste', views.teste, name='teste'),
+    path('departamentos',views.departamentos, name='departamentos'),# CAMINHO PARA A VIWS
+    path('categoria',views.categoria, name= 'categoria'),
+    path('produtos',views.produto,name='produto')
 ]
--- Store/views.py
+++ Store/views.py
@@ -1,7 +1,9 @@
 from http.client import HTTPResponse
+from math import prod
 from multiprocessing import context
 from django.shortcuts import render
-from Store.models import Departamento
+from Store.models import Departamento, Categoria, Produto
+
 
 # Create your views here.
 def index(request):
@@ -10,8 +12,28 @@ def index(request):
     context = {'nome': meu_nome, "artigo": "o" if sexo=="m" else "a"}
     return render(request,'index.html',context)
 
-def teste(request):
-    # depto = ['Casa','Informatica','Telefonia','Gamer']
+# def teste(request):
+#     # depto = ['Casa','Informatica','Telefonia','Gamer']
+#     depto = Departamento.objects.all()
+#     context = {'departamentos':depto}
+#     return render(request,'teste.html',context)
+
+# FUNCOES QUE CHAMA MOLDELS E ENVIA PARA HTML
+def departamentos(request):
     depto = Departamento.objects.all()
     context = {'departamentos':depto}
-    return render(request,'teste.html',context)
+    return render(request,'departamentos.html',context)  
+
+
+def categoria(request):
+    cat = Categoria.objects.all()
+    context = {'categoria':cat}
+    return render(request,'categoria.html',context)   
+
+def produto(request):
+    prod = Produto.objects.all()
+    context = {'produto':prod}
+    return render(request,'produtos.html',context)       
+
+
+
